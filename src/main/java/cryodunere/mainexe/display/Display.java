0202562ddb5ae9813b59943b2ecfa9893c212b5e
Kevin Ferrare <kevinferrare@gmail.com>
2021-10-26 21:18:21 +0200

    More reimplementation

--- src/main/java/cryodunere/mainexe/display/Display.java
+++ src/main/java/cryodunere/mainexe/display/Display.java
@@ -34,12 +34,14 @@ public class Display extends JavaOverrideHelper {
     defineFunction(segment, 0xD068, "setFontToIntro", this::setFontToIntro_0x1ED_0xD068_0xEF38);
     defineFunction(segment, 0xD075, "setFontToMenu", this::setFontToMenu_0x1ED_0xD075_0xEF45);
     defineFunction(segment, 0xD082, "setFontToBook", this::setFontToBook_0x1ED_0xD082_0xEF52);
+    defineFunction(segment, 0xE270, "pushAll", this::pushAll_0x1ED_0xE270_0x10140);
+    defineFunction(segment, 0xE283, "popAll", this::popAll_0x1ED_0xE283_0x10153);
   }
 
   public Runnable clearUnknownValuesAndAX_0x1ED_0x98F5_0xB7C5() {
     // Called after screen change (video, room, dialogue, map ...).
     // When set to 255, cannot enter orni and enter palace instead
-    LOGGER.info("Before: 1C06:{}, 1BF8:{}, 1BEA:{}", globalsOnDs.get1C06(), globalsOnDs.get1BF8(),
+    LOGGER.debug("Before: 1C06:{}, 1BF8:{}, 1BEA:{}", globalsOnDs.get1C06(), globalsOnDs.get1BF8(),
         globalsOnDs.get1BEA());
     globalsOnDs.set1C06(0);
     // 128 after end of dialogue if character is in the room
@@ -116,6 +118,44 @@ public class Display extends JavaOverrideHelper {
   public Runnable setFontToBook_0x1ED_0xD082_0xEF52() {
     globalsOnDs.setFontRelated2518(0xD0FF);
     globalsOnDs.set47A0(0xCEEC);
+    return nearRet();
+  }
+
+  public Runnable pushAll_0x1ED_0xE270_0x10140() {
+    LOGGER.debug("pushAll");
+    stack.push(state.getBX());
+    stack.push(state.getCX());
+    stack.push(state.getDX());
+    stack.push(state.getSI());
+    stack.push(state.getDI());
+    stack.push(state.getBP());
+    int stackTop = stack.peek(0);
+    // XCHG AX <-> Stack[0x0C]
+    int stackPeek = stack.peek(0x0C);
+    stack.poke(0x0C, state.getAX());
+    // In the original assembly code, AX seems modified but it's not the case as it's restored to its original value
+    // later.
+    stack.push(stackPeek);
+    state.setBP(stackTop);
+    return nearRet();
+  }
+
+  public Runnable popAll_0x1ED_0xE283_0x10153() {
+    LOGGER.debug("popAll");
+    // Called in most changes related to display like scene change, displaying map, clicking on map, clicking on
+    // characters ...
+    // XCHG AX <-> Stack[0x0C] (or 0x0E if done before the pop)
+    int ax = stack.pop();
+    int stackPeek = stack.peek(0x0C);
+    stack.poke(0x0C, ax);
+    state.setAX(stackPeek);
+    // Regular pops
+    state.setBP(stack.pop());
+    state.setDI(stack.pop());
+    state.setSI(stack.pop());
+    state.setDX(stack.pop());
+    state.setCX(stack.pop());
+    state.setBX(stack.pop());
     return nearRet();
   }
 }
